be8276659fa44d70b4fd7925371f7920cfd52490
Alexandre Machado <amch.smd@gmail.com>
2019-05-01 01:24:52 -0300

    WIP: Localização
    Terminei a parte de recuperar os dados de contato e salvar nas SharedPreferences. Adicionado arquivo de strings pra localização do aplicativo.

--- ContactList/app/src/main/java/com/example/aluno/contactlist/MainActivity.java
+++ ContactList/app/src/main/java/com/example/aluno/contactlist/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aluno.contactlist;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
@@ -46,38 +48,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(v.getId()){
 
             case R.id.btnCadastro:
-
-             Intent contactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+                Intent contactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
                 contactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
                 startActivityForResult(contactIntent, PICK_CONTACT_REQUEST);
                 break;
 
             case R.id.btnPesquisa:
-
                 String value = input.getText().toString();
                 for(ContatoDeEmergencia c : contactList){
                     if(value.equals(c.nome)){
-                        textView.setText("Contato: " + c.nome + " " +
-                                "[" + c.numero + "]");
+                        textView.setText(getApplicationContext().getString(R.string.contact_data, c.nome, Long.toString(c.numero)));
                         try {
-                            String phoneNumber = "tel:" + Long.toString(c.numero);
+                            String phoneNumber = "tel:" + c.numero;
                             Intent intent = new Intent(Intent.ACTION_DIAL);
                             intent.setData(Uri.parse(phoneNumber));
                             startActivity(intent);
                         } catch (SecurityException e){
                             e.printStackTrace();
+                            throw e;
                         }
                     } else {
-                        textView.setText("Contato não encontrado");
+                        textView.setText(getResources().getString(R.string.contact_not_found));
                     }
                 }
 
                 break;
         }
-
-
-
-
     }
 
     @Override
@@ -94,10 +90,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
                 cursor.moveToFirst();
 
-                // Retrieve the phone number from the NUMBER column
-                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-                String number = cursor.getString(column);
+                // Recupera o nome e o número do contato
+                String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String contactNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                this.salvarContatoNaLista(contactName, contactNumber);
             }
         }
+    }
+
+    private void salvarContatoNaLista(String nome, String numero){
+        SharedPreferences prefContato = getSharedPreferences("contato", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefContato.edit();
+        editor.putString(nome, numero);
+        // preciso jogar pra persistência direto, por isso uso commit() e não apply()
+        editor.commit();
     }
 }
